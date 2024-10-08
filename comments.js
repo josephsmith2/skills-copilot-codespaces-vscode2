519e68a0e8433d9fafaaf5929c6a927e782a8f0a
josephsmith <Josephsmith.ict@gmail.com>
2024-10-08 06:08:14 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,23 +1,8 @@
-// Create web server
-// Create a web server that listens to requests on port 3000 and serves a list of comments from comments.json when it receives a request to the path /comments. You can read the comments from comments.json using the provided readComments function.
+// create a web server
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
+var cache = {};
 
-const http = require('http');
-const fs = require('fs');
-
-const readComments = () => {
-  const comments = fs.readFileSync('./comments.json');
-  return JSON.parse(comments);
-};
-
-const server = http.createServer((req, res) => {
-  if (req.url === '/comments') {
-    const comments = readComments();
-    res.end(JSON.stringify(comments));
-  } else {
-    res.end('Not Found');
-  }
-});
-
-server.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
+// send 404
